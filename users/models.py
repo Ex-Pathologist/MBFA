7b107466888e56b2d3d2b3c8c8b8391fdb392a12
stanislau kazantsau <felixmickewich@gmail.com>
2021-12-14 15:43:10 +0300

    work with models part 1

--- users/models.py
+++ users/models.py
@@ -1,5 +1,32 @@
 from django.db import models
 
+
 class Users(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    """Пользователи"""
+
+    first_name = models.CharField("Имя", max_length=30)
+    last_name = models.CharField("Фамилия", max_length=30)
+    date_of_birthday = models.DateField()
+    sex = models.CharField("Пол", max_length=7)
+    place_of_birth = models.CharField("Место рождения", max_length=100)
+    place_of_life = models.CharField("Адрес проживания", max_length=100)
+    education = models.CharField("Образование", max_length=100)
+    profession = models.CharField("Занимаемая должность", max_length=50)
+    is_doctor = models.BooleanField(default=False)
+    id_cards = models.AutoField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Cards(models.Model):
+    """Карточки пациентов"""
+
+    id_cards = models.ForeignKey(Users, verbose_name='Номер карточки', on_delete=models.SET_NULL, null=True)
+    # id_tests = models.IntegerField(auto_created=)
+    # visites
+    # diagnosis
